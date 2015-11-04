f3d59c3acf16ad8d4da5d253f62f71ea5bd2d4dd
Dudderz <tom.dudley5@gmail.com>
2015-11-04 12:29:28 +0000

    Added the pom file and the src/ folder

---
+++ src/main/java/Entities/Employee.scala
@@ -0,0 +1,10 @@
+package Entities
+
+/**
+ * @author tdudley
+ */
+class Employee(val employeeID : Int, val employeeName : String, 
+    val username : String, val password : String) 
+{
+  
+}
---
+++ src/main/java/Main.scala
@@ -0,0 +1,76 @@
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.stage.Stage
+import scalafx.stage.StageStyle
+
+import scalafx.scene.Scene
+import scalafx.scene.layout.HBox
+import scalafx.scene.effect.DropShadow
+import scalafx.scene.paint.Color._
+import scalafx.scene.paint.LinearGradient
+import scalafx.scene.paint.Stops
+import scalafx.scene.paint.Color
+
+import scalafx.geometry.Insets
+
+import scalafx.scene.text.Text
+
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.control.TableColumn._
+import scalafx.scene.control.{TableCell, TableColumn, TableView}
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Circle
+
+/**
+ * @author tdudley
+ */
+object Main extends JFXApp {
+
+  stage = new PrimaryStage 
+  {
+    initStyle(StageStyle.UNIFIED)
+    title = "Warehouse Order Tracking Application"
+    width = 800
+    height = 600   
+    
+    scene = new Scene
+    {
+      content = new HBox
+      {
+        children = Seq(
+          new Text {
+            text = "Scala"
+            style = "-fx-font: italic bold 100pt sans-serif"
+            fill = new LinearGradient(
+               endX = 0,
+               stops = Stops(White, DarkGray)
+            )
+            
+            effect = new DropShadow{
+              color = DarkGray
+              radius = 15
+              spread - 0.25
+            }
+          },
+          new Text{
+            text = "FX"
+            style = "-fx-font: italic bold 100pt sans-serif"
+            fill = new LinearGradient(
+                endX = 0,
+                stops = Stops(White, DarkGray)
+            )
+            
+            effect = new DropShadow{
+              color = DarkGray
+              radius = 15
+              spread = 0.25
+            }
+          }
+        )
+      }
+    }
+   
+  }
+  
+}
